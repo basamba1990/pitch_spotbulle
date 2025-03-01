c1e20a319734374b823ed0803e53c542907aecf6
basamba1990 <basamba1990@yahoo.fr>
2025-03-01 14:06:47 +0000

    Update transcriber.py

--- models/transcriber.py
+++ models/transcriber.py
@@ -1,15 +1,22 @@
-from google.cloud import speech_v1p1beta1 as speech
+from dotenv import load_dotenv
 import os
+from google.cloud import speech_v1p1beta1 as speech
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Utiliser la variable GOOGLE_APPLICATION_CREDENTIALS pour définir la clé de service
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 def transcribe_video(video_path: str) -> str:
     """
     Transcrit l'audio d'une vidéo en texte en utilisant Google Speech-to-Text.
-    
+
     Parameters
     ----------
     video_path : str
         Chemin vers le fichier vidéo.
-    
+
     Returns
     -------
     str

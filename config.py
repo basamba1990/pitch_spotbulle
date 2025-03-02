13ace66495e308b9972ec45a08427bfca2138d06
basamba1990 <basamba1990@yahoo.fr>
2025-03-02 08:22:22 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")  # Récupère la clé secrète
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")  # Dossier pour les fichiers téléchargés
     # Ajouter d'autres variables nécessaires
-    API_KEY_TRANSCRIPTION = os.getenv("API_KEY_TRANSCRIPTION")
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")  # Ajouté pour la clé Google Cloud
 
 
